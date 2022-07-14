9e39a2a7aba723f3ada2e8395add5d829d7583fc
albuquerquefabio <albuquerquefabio@icloud.com>
2022-07-14 18:53:33 -0300

    start import disable camelcase

--- packages/rest-typings/src/v1/import/StartImportParamsPOST.ts
+++ packages/rest-typings/src/v1/import/StartImportParamsPOST.ts
@@ -8,34 +8,23 @@ export type StartImportParamsPOST = {
 	input: {
 		users: [
 			{
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				user_id: string;
 				username: string;
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				email: string;
 				is_deleted: boolean;
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				is_bot: boolean;
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				do_import: boolean;
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				is_email_taken: boolean;
 			},
 		];
 		channels: [
 			{
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				channel_id: string;
-
 				name: string;
 				creator?: string;
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				is_archived: boolean;
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				do_import: boolean;
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				is_private: boolean;
-				// eslint-disable-next-line @typescript-eslint/camelcase
 				is_direct: boolean;
 			},
 		];
@@ -53,12 +42,17 @@ const StartImportParamsPostSchema = {
 					items: {
 						type: 'object',
 						properties: {
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							user_id: { type: 'string' },
 							username: { type: 'string' },
 							email: { type: 'string' },
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							is_deleted: { type: 'boolean' },
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							is_bot: { type: 'boolean' },
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							do_import: { type: 'boolean' },
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							is_email_taken: { type: 'boolean' },
 						},
 						required: ['user_id', 'username', 'email', 'is_deleted', 'is_bot', 'do_import', 'is_email_taken'],
@@ -69,12 +63,17 @@ const StartImportParamsPostSchema = {
 					items: {
 						type: 'object',
 						properties: {
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							channel_id: { type: 'string' },
 							name: { type: 'string' },
 							creator: { type: 'string' },
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							is_archived: { type: 'boolean' },
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							do_import: { type: 'boolean' },
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							is_private: { type: 'boolean' },
+							// eslint-disable-next-line @typescript-eslint/camelcase
 							is_direct: { type: 'boolean' },
 						},
 						required: ['channel_id', 'name', 'is_archived', 'do_import', 'is_private', 'is_direct'],
